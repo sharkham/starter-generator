21375cc6b196d410f52d9bd27ac819e356d03208
Sam Markham <sam.e.markham@gmail.com>
2019-08-02 12:56:43 -0700

    Added random_number method to Generator class

--- lib/environment.rb
+++ lib/environment.rb
@@ -2,6 +2,7 @@ require_relative "./starter_generator/version"
 require_relative "./starter_generator/cli"
 require_relative "./starter_generator/pokemon"
 require_relative "./starter_generator/scraper"
+require_relative "./starter_generator/generator"
 require 'pry'
 
 module StarterGenerator
--- lib/starter_generator/generator.rb
+++ lib/starter_generator/generator.rb
@@ -3,10 +3,14 @@ class Generator
 
   def self.random_number
     #generate a random number from 0-[Scraper.all_pokemon.length]
+    rand(1..(Scraper.all_pokemon.length))
   end
 
+  #should this be in the Pokémon class? Should Pokémon be responsible for creating themselves? Yes probably.
   def self.make_pokemon
     #use the number generated in the previous method to pick a Pokémon from that array
     #instantiate a new Pokémon object with the name and the number(+1)
   end
 end
+#rand(1..(Scraper.all_pokemon.length))
+#rand(1..(Scraper.all_pokemon.length))
