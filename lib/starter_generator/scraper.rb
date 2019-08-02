99d73d408dc4633d6fcfca717a52ac650ff728b3
Sam Markham <sam.e.markham@gmail.com>
2019-08-02 11:36:11 -0700

    Got Scraper .scrape_list_page method working

--- lib/starter_generator/scraper.rb
+++ lib/starter_generator/scraper.rb
@@ -2,14 +2,27 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-class StarterGenerator::Scraper
+class Scraper
+  @@all_pokemon
+
+  def self.all
+    @@all_pokemon
+  end
+
   def self.scrape_list_page
     scraped_pokemon = []
     #this bit maybe shouldn't be hardcoded but not sure how else to do it since scraper is only good for this site
-    url = open("https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number")
+
+    url = open("https://www.serebii.net/pokemon/all.shtml")
+
     index_page = Nokogiri::HTML(url)
     binding.pry
-    index_page.css(table[align="center"])
+    # all_pokemon = index_page.css("table.dextable tr:nth-of-type(1n+3) td:nth-of-type(3) a").text
+    pokemon_scrape = index_page.css("table.dextable tr td:nth-of-type(3)").each do |pokemon|
+      name = pokemon.css("a").text
+      @@all_pokemon << name
+    end
+    @@all_pokemon.delete_if {|pokemon| pokemon == ""}
   end
 
   def self.scrape_pokemon_page(pokemon_name)
@@ -17,3 +30,17 @@ class StarterGenerator::Scraper
   end
 end
 
+#index_page.css("table tr td a").text - gets literally all the Pokémon and types, mostly all in one word.
+#could try to use regex to split it into an array and then exclude all words that are types? But then
+#still get a bunch of garbage at the end
+
+#index_page.css("table tr:nth-child(2) td:nth-child(4) a").text = "BulbasaurChikoritaTreeckoTurtwigVictiniChespinRowletGrookey"
+#it's grabbing all of the Pokémon in tables in that position--so, all starters in each dex.
+
+#SEREBII
+
+#serebii test: serebii_test_2.css("table.dextable tr:nth-of-type(1n+3) td:nth-of-type(3) a").text
+#serebii test 2: serebii_test_2.css("table.dextable tr td:nth-of-type(3) a").text
+
+#index_page.css("table.dextable tr td:nth-of-type(3)").text
+#gets: "\r\n\t\tName\t\r\n\t\tDef\r\n\t\tBulbasaur\r\n\t\t\r\n\t\tIvysaur\r\n\t\t\r\n\t\tVenusaur\r\n\t\t\r\n\t\tCharmander\r\n\t\t\r\n\t\tCharmeleon\r\n\t\t\r\n\t\tCharizard\r\n\t\t\r\n\t\tSquirtle"
