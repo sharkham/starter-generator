8aaa7c5bb3cd58c1c6c203696b532c3f1c37f62a
Sam Markham <sam.e.markham@gmail.com>
2019-08-06 11:53:59 -0700

    Put description and types into hash.

--- lib/starter_generator/scraper.rb
+++ lib/starter_generator/scraper.rb
@@ -10,23 +10,21 @@ class Scraper
   end
 
   def self.scrape_list_page
-    # scraped = []
     url = open("https://www.serebii.net/pokemon/all.shtml")
 
     index_page = Nokogiri::HTML(url)
 
-    # all = index_page.css("table.dextable tr:nth-of-type(1n+3) td:nth-of-type(3) a").text
     pokemon_scrape = index_page.css("table.dextable tr td:nth-of-type(3)").each do |pokemon|
       name = pokemon.css("a").text
       @@all << name
     end
     @@all.delete_if {|pokemon| pokemon == ""}
     @@all.delete_if.with_index { |pokemon, i| i > 720 }
-    # @@all.delete_if {|pokemon| pokemon.index > 721}
   end
 
 
   def self.scrape_pokemon_page(number)
+    #this can be one helper method: number conversion
     if number < 10
       url_number = "00#{number.to_s}"
     elsif number < 100
@@ -36,28 +34,33 @@ class Scraper
     else
       puts "error"
     end
+    attribute_hash = {}
+    #this can be one helper method, getting the first level scrape I use for the other scrapes?
     url = open("https://www.serebii.net/pokedex-xy/#{url_number}.shtml")
     index_page = Nokogiri::HTML(url)
     first_level = index_page.css("table:nth-of-type(2) tr:nth-of-type(2) td:nth-of-type(2) div:nth-of-type(2) div")
+    #helper method description scrape
     description_array = []
     description_scrape = first_level.css("td.ruby").each do |text|
       words = text.css("~ td.fooinfo").text
       description_array << words
     end
-    description = description_array[1]
-
+    attribute_hash[:description] = description_array[1]
+    #helper method type scrape
     text_array = []
     type_scrape = first_level.css("td.cen a").each do |foo|
       type = foo.attribute("href").text
       text_array << type
     end
     types_array = text_array.select {|text| text.include?("pokedex-xy")}
-    better_types_array = types_array.collect {|type| type.slice(12..-7)}
+    attribute_hash[:types_array] = types_array.collect {|type| type.slice(12..-7)}
+    attribute_hash
 
-    type_test = type_scrape.slice(12..-7)
 
 
-    #stick a hash in here at some point, and possible build these specific scrapes out as helper methods
+    #stick a hash in here at some point, and possible build these specific scrapes out as helper methods?
+    #or at least do the number conversion as helper method.
+    #REFACTOR LATER, GET WORKING NOW.
 
 
 
