cf9992c2e3c25e295b255418cf69ea10f1daf7b2
Sam Markham <sam.e.markham@gmail.com>
2019-08-06 14:46:33 -0700

    CLI can now print Pokémon info

--- lib/starter_generator/cli.rb
+++ lib/starter_generator/cli.rb
@@ -8,6 +8,7 @@ class StarterGenerator::CLI
     make_three_pokemon
     pokemon_options
     choose
+    more_info
     goodbye
   end
 
@@ -52,16 +53,35 @@ class StarterGenerator::CLI
       input = gets.strip.downcase
       if input.to_i > 0 && input.to_i < 4
         the_pokemon = @pokemon[input.to_i-1]
-        #this is where I want to use this to do a different method that's populating the Pokémon object
-        puts "#{the_pokemon.name}"
+        more_info(the_pokemon)
+        print_pokemon_info(the_pokemon)
+        #this breaks my going back to list or exit! It says it's running more_info and needs an argument?
+
+        # puts "#{the_pokemon.name}"
       elsif input == "list"
         pokemon_options
       elsif input == "exit"
+        #this isn't working--something needs to happen here. Break? We don't wanna use that though.
       else
         puts "What was that? Please type list, exit, or a Pokémon number."
         #this is an error--this comes up when you input "exit" and then it does "goodbye"
       end
     end
+  end
+
+  def more_info(pokemon)
+    attributes = Scraper.scrape_pokemon_page(pokemon.number)
+    pokemon.add_scraped_attributes(attributes)
+  end
+
+  def print_pokemon_info(pokemon)
+    puts "  Name: #{pokemon.name}"
+    puts "  Number: #{pokemon.number}"
+    #make sure the number thing is formatted properly here so it reads right.
+    puts "  Type: #{pokemon.type}"
+    #this needs to not be an array here
+    puts "  Description: #{pokemon.description}"
+    puts "  This Pokémon has a #{pokemon.nature} nature."
   end
 
   def goodbye
