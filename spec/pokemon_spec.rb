a947d98705a3e44160ee82d05787f20ab4731070
Sam Markham <sam.e.markham@gmail.com>
2019-08-08 11:47:16 -0700

    Added nature attribute to pokemon_spec.rb

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -1,18 +1,18 @@
 require "spec_helper"
 
 describe "Pokemon" do
-  let(:pokemon) { Pokemon.new("Tyranitar", 248) }
+  let(:pokemon) { Pokemon.new("Tyranitar", 248, "Naughty") }
 
   describe "#initialize" do
     it "accepts a name for the new pokemon" do
-      new_pokemon = Pokemon.new("Litwick", 607)
+      new_pokemon = Pokemon.new("Litwick", 607, "Mild")
 
       new_pokemon_name = new_pokemon.instance_variable_get(:@name)
 
       expect(new_pokemon_name).to eq("Litwick")
     end
     it "accepts a number for the new pokemon" do
-      new_pokemon_2 = Pokemon.new("Bulbasaur", 1)
+      new_pokemon_2 = Pokemon.new("Bulbasaur", 1, "Timid")
 
       new_pokemon_number = new_pokemon_2.instance_variable_get(:@number)
 
@@ -35,6 +35,16 @@ describe "Pokemon" do
       expect(pokemon_name).to eq("Litwick")
     end
   end
+
+  describe "#nature=" do
+  it "can set the nature of a pokemon" do
+    pokemon.nature = "Mild"
+
+    pokemon_nature = pokemon.instance_variable_get(:@nature)
+
+    expect(pokemon_nature).to eq("Mild")
+  end
+end
 
   describe "@@all" do
     it "is initialized as an empty array" do
@@ -74,7 +84,7 @@ describe "Pokemon" do
 
   describe ".create" do
     it "initializes, saves, and returns the pokemon" do
-      created_pokemon = Pokemon.create("Charmander", 4)
+      created_pokemon = Pokemon.create("Charmander", 4, "Bold")
 
       expect(Pokemon.all).to include(created_pokemon)
     end
