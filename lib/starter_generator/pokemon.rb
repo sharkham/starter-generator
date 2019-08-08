c27076c41d79ad21117ae6ff616c854f778944da
Sam Markham <sam.e.markham@gmail.com>
2019-08-08 14:05:37 -0700

    Removed test code from Pokemon class

--- lib/starter_generator/pokemon.rb
+++ lib/starter_generator/pokemon.rb
@@ -51,36 +51,6 @@ class Pokemon
     self
   end
 
-  def self.generated
-    #should return three instances of Pokémon
-    #scrape Bulbapedia and return Pokémon generated through that list
-
-    #THESE NEED TO BE COMMENTED OUT TO PASS TEST BUT ON TO RUN BIN/RUN
-
-    # pokemon_1 = self.new("Test", 3)
-    # pokemon_1.name = "Tyranitar"
-    # pokemon_1.type = "Rock/Dark"
-    # pokemon_1.description = "Beautiful big boy."
-    # pokemon_1.nature = "Adamant"
-    # pokemon_1.level = "60"
-
-    # pokemon_2 = self.new("Test", 4)
-    # pokemon_2.name = "Litwick"
-    # pokemon_2.type = "Ghost/Fire"
-    # pokemon_2.description = "Smol candle friend."
-    # pokemon_2.nature = "Lonely"
-    # pokemon_2.level = "20"
-
-    # pokemon_3 = self.new("Test", 5)
-    # pokemon_3.name = "Sunkern"
-    # pokemon_3.type = "Grass"
-    # pokemon_3.description = "Happy plant."
-    # pokemon_3.nature = "Lazy"
-    # pokemon_3.level = "15"
-
-    # [pokemon_1, pokemon_2, pokemon_3]
-  end
-
 end
 
 
